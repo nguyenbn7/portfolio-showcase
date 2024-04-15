a766eb3f4861d1556db3a7c41d06a3dd8f151419
nguyenbn7 <nguyenbn.email@gmail.com>
2024-04-16 00:07:46 +0700

    done style resume

---
+++ src/lib/home/resume-section.svelte
@@ -0,0 +1,135 @@
+<section id="resume">
+	<div class="container" data-aos="fade-up">
+		<div class="section-title">
+			<h2>Resume</h2>
+			<p>
+				Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem.
+				Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit
+				alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
+			</p>
+		</div>
+
+		<div class="row">
+			<div class="w-full md:w-1/2">
+				<h3 class="text-[26px] font-bold my-[20px] text-[#45505b]">Sumary</h3>
+				<div
+					class="relative pl-[20px] pb-[20px] -mt-[2px] border-l-2 border-solid border-[#0563bb] before:content-[''] before:absolute before:size-[16px] before:rounded-[50px] before:-left-[9px] before:top-0 before:bg-white before:border-[2px] before:border-solid before:border-[#0563bb]"
+				>
+					<h4 class="mb-[10px] text-[18px] leading-[18px] font-semibold uppercase text-[#0563bb]">
+						Brandon Johnson
+					</h4>
+					<p>
+						<em
+							>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing
+							and developing user-centered digital/print marketing material from initial concept to
+							final, polished deliverable.</em
+						>
+					</p>
+					<ul class="pl-[20px] list-disc">
+						<li class="pb-[10px] last:pb-0">Portland par 127,Orlando, FL</li>
+						<li class="pb-[10px] last:pb-0">(123) 456-7891</li>
+						<li class="pb-[10px] last:pb-0">alice.barkley@example.com</li>
+					</ul>
+				</div>
+
+				<h3 class="text-[26px] font-bold my-[20px] text-[#45505b]">Education</h3>
+				<div
+					class="relative pl-[20px] pb-[20px] -mt-[2px] border-l-2 border-solid border-[#0563bb] before:content-[''] before:absolute before:size-[16px] before:rounded-[50px] before:-left-[9px] before:top-0 before:bg-white before:border-[2px] before:border-solid before:border-[#0563bb]"
+				>
+					<h4 class="mb-[10px] text-[18px] leading-[18px] font-semibold uppercase text-[#0563bb]">
+						Master of Fine Arts &amp; Graphic Design
+					</h4>
+					<h5
+						class="text-[16px] py-[5px] px-[15px] inline-block font-semibold mb-[10px] bg-[#f7f8f9]"
+					>
+						2015 - 2016
+					</h5>
+					<p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+					<p>
+						Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero
+						voluptatum qui ut dignissimos deleniti nerada porti sand markend
+					</p>
+				</div>
+				<div
+					class="relative pl-[20px] pb-[20px] -mt-[2px] border-l-2 border-solid border-[#0563bb] before:content-[''] before:absolute before:size-[16px] before:rounded-[50px] before:-left-[9px] before:top-0 before:bg-white before:border-[2px] before:border-solid before:border-[#0563bb]"
+				>
+					<h4 class="mb-[10px] text-[18px] leading-[18px] font-semibold uppercase text-[#0563bb]">
+						Bachelor of Fine Arts &amp; Graphic Design
+					</h4>
+					<h5
+						class="text-[16px] py-[5px] px-[15px] inline-block font-semibold mb-[10px] bg-[#f7f8f9]"
+					>
+						2010 - 2014
+					</h5>
+					<p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+					<p>
+						Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius
+						vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae
+						consequatur neque etlon sader mart dila
+					</p>
+				</div>
+			</div>
+			<div class="w-full md:w-1/2">
+				<h3 class="text-[26px] font-bold my-[20px] text-[#45505b]">Professional Experience</h3>
+				<div
+					class="relative pl-[20px] pb-[20px] -mt-[2px] border-l-2 border-solid border-[#0563bb] before:content-[''] before:absolute before:size-[16px] before:rounded-[50px] before:-left-[9px] before:top-0 before:bg-white before:border-[2px] before:border-solid before:border-[#0563bb]"
+				>
+					<h4 class="mb-[10px] text-[18px] leading-[18px] font-semibold uppercase text-[#0563bb]">
+						Senior graphic design specialist
+					</h4>
+					<h5
+						class="text-[16px] py-[5px] px-[15px] inline-block font-semibold mb-[10px] bg-[#f7f8f9]"
+					>
+						2019 - Present
+					</h5>
+					<p><em>Experion, New York, NY </em></p>
+					<ul class="pl-[20px] list-disc">
+						<li class="pb-[10px] last:pb-0">
+							Lead in the design, development, and implementation of the graphic, layout, and
+							production communication materials
+						</li>
+						<li class="pb-[10px] last:pb-0">
+							Delegate tasks to the 7 members of the design team and provide counsel on all aspects
+							of the project.
+						</li>
+						<li class="pb-[10px] last:pb-0">
+							Supervise the assessment of all graphic materials in order to ensure quality and
+							accuracy of the design
+						</li>
+						<li class="pb-[10px] last:pb-0">
+							Oversee the efficient use of production project budgets ranging from $2,000 - $25,000
+						</li>
+					</ul>
+				</div>
+				<div
+					class="relative pl-[20px] pb-[20px] -mt-[2px] border-l-2 border-solid border-[#0563bb] before:content-[''] before:absolute before:size-[16px] before:rounded-[50px] before:-left-[9px] before:top-0 before:bg-white before:border-[2px] before:border-solid before:border-[#0563bb]"
+				>
+					<h4 class="mb-[10px] text-[18px] leading-[18px] font-semibold uppercase text-[#0563bb]">
+						Graphic design specialist
+					</h4>
+					<h5
+						class="text-[16px] py-[5px] px-[15px] inline-block font-semibold mb-[10px] bg-[#f7f8f9]"
+					>
+						2017 - 2018
+					</h5>
+					<p><em>Stepping Stone Advertising, New York, NY</em></p>
+					<ul class="pl-[20px] list-disc">
+						<li class="pb-[10px] last:pb-0">
+							Developed numerous marketing programs (logos, brochures,infographics, presentations,
+							and advertisements).
+						</li>
+						<li class="pb-[10px] last:pb-0">
+							Managed up to 5 projects or tasks at a given time while under pressure
+						</li>
+						<li class="pb-[10px] last:pb-0">
+							Recommended and consulted with clients on the most appropriate graphic design
+						</li>
+						<li class="pb-[10px] last:pb-0">
+							Created 4+ design presentations and proposals a month for clients and account managers
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
