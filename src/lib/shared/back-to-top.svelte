30c630afd62d70e908b8c2f04e6ff27235df6431
nguyenbn7 <nguyenbn.email@gmail.com>
2024-04-15 09:09:06 +0700

    add font awesome, back to top btn

---
+++ src/lib/shared/back-to-top.svelte
@@ -0,0 +1,60 @@
+<script>
+	import { expoInOut } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+	import { fade } from 'svelte/transition';
+
+	/**
+	 * Millisecond
+	 * @default 0
+	 */
+	export let delay = 0;
+	/**
+	 * Millisecond
+	 * @default 400
+	 */
+	export let duration = 400;
+	/**
+	 * See {@link https://svelte.dev/docs/svelte-easing}
+	 * @default expoInOut
+	 */
+	export let easing = expoInOut;
+	/**
+	 * @default 50px
+	 */
+	export let showThreshold = 50;
+
+	/**
+	 * @type {number}
+	 */
+	let scrollY;
+	const a = tweened(0);
+
+	/**
+	 * @param {number} delay
+	 * @param {number} duration
+	 * @param {(t: number) => number} easing
+	 */
+	async function backToTop(delay, duration, easing) {
+		a.set(scrollY, { duration: 0 });
+
+		const unsubscribe = a.subscribe((value) => {
+			window.scrollTo(0, value);
+		});
+
+		await a.set(0, { delay, duration, easing });
+
+		return unsubscribe();
+	}
+</script>
+
+<svelte:window bind:scrollY />
+
+<a
+	href={'javascript:;'}
+	on:click={() => backToTop(delay, duration, easing)}
+	class="fixed bottom-[15px] right-[15px] z-[996] size-[40px] bg-[#0563bb] hover:bg-[#0678e3] rounded-[50px] animate-bounce flex items-center justify-center text-white text-[16px] leading-0"
+	class:invisible={scrollY <= showThreshold}
+	transition:fade={{ duration: 1000 }}
+>
+	<slot />
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
 	import '../app.css';
+	import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+	import '@fortawesome/fontawesome-free/css/regular.min.css';
+	import '@fortawesome/fontawesome-free/css/solid.min.css';
+	import '@fortawesome/fontawesome-free/css/brands.min.css';
+	import BackToTop from '$lib/shared/back-to-top.svelte';
 </script>
 
 <slot />
+<BackToTop>
+	<i class="fa-solid fa-arrow-up-long"></i>
+</BackToTop>
