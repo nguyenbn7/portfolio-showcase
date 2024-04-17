74b4dab6f3528c7dd6f5382155bc7a9aa655df52
nguyenbn7 <nguyenbn.email@gmail.com>
2024-04-17 23:41:17 +0700

    add click outside and hide

--- src/lib/shared/toggle-dark-mode.svelte
+++ src/lib/shared/toggle-dark-mode.svelte
@@ -1,14 +1,16 @@
 <script>
 	import { onMount } from 'svelte';
 
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	let ele;
 	let hidden = true;
 
 	/**
 	 * @param {'dark' | 'light' | 'system'} option
 	 */
 	function toggleDarkMode(option) {
-		console.log(option);
-
 		if (option === 'light' && document.documentElement.classList.contains('dark')) {
 			document.documentElement.classList.remove('dark');
 			localStorage.theme = 'light';
@@ -40,11 +42,21 @@
 			document.documentElement.classList.remove('dark');
 		}
 	});
+
+	/**
+	 * @param {MouseEvent & { currentTarget: EventTarget & Window; }} $event
+	 */
+	function hideList($event) {
+		// @ts-ignore
+		if (!ele.contains($event.target)) hidden = true;
+	}
 </script>
 
-<div class="relative">
+<svelte:window on:click={hideList} />
+
+<div class="relative" bind:this={ele}>
 	<button
-		class="size-[42px] text-[20px] flex items-center justify-center bg-primary-original text-white-smoke rounded-[10px] p-[18px] outline-none"
+		class="size-[42px] text-[20px] flex items-center justify-center bg-primary-original text-white-smoke hover:bg-primary-400 active:bg-primary-900 rounded-[10px] p-[18px] outline-none"
 		title="Dark Mode"
 		on:click={() => (hidden = !hidden)}
 	>
@@ -56,17 +68,26 @@
 	>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-		<li class="px-4 py-2 text-sm hover:bg-primary-400" on:click={() => toggleDarkMode('light')}>
+		<li
+			class="px-4 py-2 text-sm hover:bg-primary-400 active:bg-primary-600"
+			on:click|stopPropagation={() => toggleDarkMode('light')}
+		>
 			<i class="fa-regular fa-sun mr-3"></i> Light
 		</li>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-		<li class="pl-5 px-4 py-2 text-sm hover:bg-primary-400" on:click={() => toggleDarkMode('dark')}>
+		<li
+			class="pl-5 px-4 py-2 text-sm hover:bg-primary-400 active:bg-primary-600"
+			on:click|stopPropagation={() => toggleDarkMode('dark')}
+		>
 			<i class="fa-regular fa-moon mr-3"></i> Dark
 		</li>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-		<li class="px-4 py-2 text-sm hover:bg-primary-400" on:click={() => toggleDarkMode('system')}>
+		<li
+			class="px-4 py-2 text-sm hover:bg-primary-400 active:bg-primary-600"
+			on:click|stopPropagation={() => toggleDarkMode('system')}
+		>
 			<i class="fa-solid fa-desktop mr-3"></i> System
 		</li>
 	</ul>
